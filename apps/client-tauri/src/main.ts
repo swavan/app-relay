264a565df42bf351de0d3019198802a0e9a78a61
Biplab Pokhrel <biplabpokhrel@protonmail.com>
2026-05-02 16:52:31 -0500

    fix: use Svelte 5 mount API

--- apps/client-tauri/src/main.ts
+++ apps/client-tauri/src/main.ts
@@ -1,9 +1,9 @@
+import { mount } from "svelte";
 import App from "./App.svelte";
 import "./theme.css";
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById("app") as HTMLElement
 });
 
 export default app;
-
